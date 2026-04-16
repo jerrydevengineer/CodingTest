e55930a6634aa519a3d5bb8796622eab38ff8bfb
jerrydevengineer <jerrydevengineer@gmail.com>
2026-04-16 10:25:19 +0900

    fix

--- java/baekjoon/SwitchOnAndOff.java
+++ java/baekjoon/SwitchOnAndOff.java
@@ -10,11 +10,12 @@ public class SwitchOnAndOff {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        S = Integer.parseInt(st.nextToken());
-        on = new boolean[S+1];
+        S = Integer.parseInt(br.readLine()); // 스위치 개수 입력받기 (수정)
+        on = new boolean[S + 1];
+
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= S; i++) {
             if(Integer.parseInt(st.nextToken()) == 1) {
@@ -32,11 +33,17 @@ public class SwitchOnAndOff {
 
         br.close();
 
-        for(boolean o : on) {
-            if(o) {
-                bw.write(1 + " ");
+        // 🚨 3번, 4번 오류 수정: 1번부터 시작하며, 20개마다 줄바꿈
+        for (int i = 1; i <= S; i++) {
+            if (on[i]) {
+                bw.write("1 ");
             } else {
-                bw.write(0 + " ");
+                bw.write("0 ");
+            }
+
+            // 20번째 스위치마다 줄바꿈
+            if (i % 20 == 0) {
+                bw.newLine();
             }
         }
         bw.flush();
@@ -44,35 +51,33 @@ public class SwitchOnAndOff {
     }
 
     public static void changeSwitch(int s, int n) {
-        if(s == 1){
-            while(n <= on.length) {
-                toggleSwitch(n);
-                n += n;
+        if (s == 1) { // 남학생
+            // 🚨 1번 오류 수정: 배수 증가를 안전하게 처리
+            for (int i = n; i <= S; i += n) {
+                toggleSwitch(i);
             }
-        } else {
-            if(n == 1) return;
-            int l = n;
-            int r = n;
-            int max = Math.min(l-1, S - r);
-            int i = 0;
-            while(i < max) {
-                if(on[l] == on[r]) {
+        } else { // 여학생
+            // 🚨 2번 오류 수정: 자기 자신은 무조건 변경
+            toggleSwitch(n);
+
+            int l = n - 1;
+            int r = n + 1;
+
+            // 좌우로 퍼지며 대칭 검사
+            while (l >= 1 && r <= S) {
+                if (on[l] == on[r]) { // 대칭이면 양쪽 다 변경
                     toggleSwitch(l);
                     toggleSwitch(r);
                     l--;
                     r++;
                 } else {
-                    break;
+                    break; // 대칭이 깨지면 즉시 중단
                 }
             }
         }
     }
 
     public static void toggleSwitch(int i) {
-        if(on[i]) {
-            on[i] = false;
-        } else {
-            on[i] = true;
-        }
+        on[i] = !on[i]; // if-else보다 짧고 세련된 토글 방식
     }
 }
