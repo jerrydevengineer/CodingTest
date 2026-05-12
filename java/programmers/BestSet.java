e794f385fd265c318e531c94eeebc1c36cfa1cd5
jerrydevengineer <jerrydevengineer@gmail.com>
2026-05-12 13:12:25 +0900

    BestSet

--- java/programmers/BestSet.java
+++ java/programmers/BestSet.java
@@ -5,17 +5,31 @@ import java.util.List;
 
 public class BestSet {
     public int[] solution(int n, int s) {
+
         if(n > s) return new int[]{-1};
-        List<Integer> res = new ArrayList<>();
+
+        int[] res = new int[n];
         int r = s % n;
         int q = s / n;
         for(int i = 0; i < n - r; i++) {
-            res.add(q);
+            res[i] = q;
         }
         q++;
-        for(int i = 0; i < r; i++) {
-            res.add(q);
+        for(int i = n - r; i < n; i++) {
+            res[i] = q;
         }
-        return res.stream().mapToInt(Integer::intValue).toArray();
+        return res;
+
+//        List<Integer> res = new ArrayList<>();
+//        int r = s % n;
+//        int q = s / n;
+//        for(int i = 0; i < n - r; i++) {
+//            res.add(q);
+//        }
+//        q++;
+//        for(int i = 0; i < r; i++) {
+//            res.add(q);
+//        }
+//        return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
