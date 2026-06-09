926e339b19bff71bc3e2bbea39dd15efab84e667
jerrydevengineer <jerrydevengineer@gmail.com>
2026-06-09 15:34:42 +0900

    BadUser

--- java/programmers/BadUser.java
+++ java/programmers/BadUser.java
@@ -1,104 +1,49 @@
 package programmers;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class BadUser {
-    static Ss[] sList;
+    // 중복된 제재 아이디 목록을 걸러낼 HashSet
+    Set<Set<String>> resultSet = new HashSet<>();
 
     public int solution(String[] user_id, String[] banned_id) {
-        int answer = 0;
-        sList = new Ss[26];
-        for (String id : user_id) {
-            char[] temp = id.toCharArray();
-            Ss cur = null;
-            Ss[] curList = sList;
-            for (char t : temp) {
-                if (curList[t - 'a'] != null) {
-                    cur = curList[t - 'a'];
-                } else {
-                    cur = new Ss(t);
-                    curList[t - 'a'] = cur;
-                }
-                curList = cur.next;
-            }
-            if (cur != null)
-                cur.finished = true;
+        // banned_id의 '*'를 정규식 '.'으로 변환 ("fr*d*" -> "fr.d.")
+        for (int i = 0; i < banned_id.length; i++) {
+            banned_id[i] = banned_id[i].replace("*", ".");
         }
 
-        List<List<String>> ids = new ArrayList<>();
-        for (String id : banned_id) {
-            char[] temp = id.toCharArray();
-            List<String> resList = new ArrayList<>();
-            StringBuilder sb = new StringBuilder();
-            backtracking(null, temp, 0, sb, resList);
-            ids.add(resList);
-        }
+        // DFS 탐색 시작 (현재 고른 아이디들을 담을 Set, 유저목록, 제재목록, 현재 제재목록 인덱스)
+        dfs(new HashSet<>(), user_id, banned_id, 0);
 
-        for (int i = 0; i < ids.size() - 1; i++) {
-            int i1 = ids.get(i).size();
-            int i2 = ids.get(i + 1).size();
-            Set<String> set = new HashSet<>(ids.get(i));
-            set.addAll(ids.get(i + 1));
-            answer += i1 * i2 - (i1 + i2 - set.size());
-        }
-
-        return answer;
+        // 최종적으로 걸러진 고유한 조합의 개수가 정답
+        return resultSet.size();
     }
 
-    public void backtracking(Ss ss, char[] cArr, int idx, StringBuilder sb, List<String> res) {
-        if (idx == cArr.length - 1) {
-            if (ss.finished) {
-                sb.append(ss.c);
-                res.add(sb.toString());
-            } else {
-                return;
-            }
-        }
-
-        char c = cArr[idx];
-
-        if (idx == 0) {
-            if (c == '*') {
-                for (Ss first : sList) {
-                    sb.append(first.c);
-                    backtracking(first, cArr, idx + 1, sb, res);
-                    sb.deleteCharAt(0);
-                }
-            } else {
-                ss = sList[c - 'a'];
-                if (ss == null)
-                    return;
-                sb.append(ss.c);
-                backtracking(ss, cArr, idx + 1, sb, res);
-            }
+    private void dfs(Set<String> currentSet, String[] user_id, String[] banned_id, int bIdx) {
+        // 모든 banned_id에 대해 매핑을 끝냈다면
+        if (bIdx == banned_id.length) {
+            // 결과 Set에 현재 조합을 복사해서 넣음 (깊은 복사)
+            resultSet.add(new HashSet<>(currentSet));
             return;
         }
 
-        if (c == '*') {
-            for (Ss s : ss.next) {
-                sb.append(s.c);
-                backtracking(s, cArr, idx + 1, sb, res);
-                sb.deleteCharAt(sb.length());
+        // 현재 매핑해야 할 불량 사용자 패턴
+        String regex = banned_id[bIdx];
+
+        // 모든 유저 아이디를 순회하면서 매핑 가능한지 확인
+        for (String user : user_id) {
+            // 아직 이번 조합에서 선택되지 않은 유저이고, 패턴에 일치한다면
+            if (!currentSet.contains(user) && user.matches(regex)) {
+                // 선택 (방문 처리)
+                currentSet.add(user);
+
+                // 다음 불량 사용자 매핑을 위해 DFS 재귀 호출
+                dfs(currentSet, user_id, banned_id, bIdx + 1);
+
+                // 백트래킹 (다른 경우의 수를 찾기 위해 선택 해제)
+                currentSet.remove(user);
             }
-        } else {
-            if (ss.next[c - 'a'] == null)
-                return;
-            backtracking(ss.next[c - 'a'], cArr, idx + 1, sb.append(c), res);
-        }
-    }
-
-    class Ss {
-
-        char c;
-        Ss[] next;
-        boolean finished;
-
-        public Ss(char c) {
-            this.c = c;
-            next = new Ss[26];
         }
     }
 }
