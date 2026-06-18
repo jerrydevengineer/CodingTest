d2897d8070766ad65a907b5f53b62e9383848d7d
jerrydevengineer <jerrydevengineer@gmail.com>
2026-06-18 11:29:13 +0900

    JewelShopping

--- java/programmers/JewelShopping.java
+++ java/programmers/JewelShopping.java
@@ -1,25 +1,32 @@
 package programmers;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class JewelShopping {
     public int[] solution(String[] gems) {
         int[] answer = new int[2];
         Set<String> set = new HashSet<>();
-        for(String s : gems) {
+        for (String s : gems) {
             set.add(s);
         }
         int x = set.size();
-        if(x == 1) return new int[]{1, 1};
+        if (x == 1)
+            return new int[] {1, 1};
         int l = 0, r = 0, min = Integer.MAX_VALUE;
         Map<String, Integer> map = new HashMap<>();
-        while(r < gems.length && l < gems.length) {
-            while(map.size() == x) {
-                if(r - l < min) {
+        while (r < gems.length && l < gems.length) {
+            while (map.size() == x) {
+                if (r - l < min) {
                     min = r - l;
                     answer[0] = l + 1;
                     answer[1] = r + 1;
                 }
                 map.put(gems[l], map.get(gems[l]) - 1);
-                if(map.get(gems[l]) == 0) map.remove(gems[l]);
+                if (map.get(gems[l]) == 0)
+                    map.remove(gems[l]);
                 l++;
             }
             r++;
